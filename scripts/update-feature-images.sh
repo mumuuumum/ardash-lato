47d1029400644e5915b56bc5eb1ba8a1061f1b72
Andreas Redmer <ar-gitlab@abga.be>
2023-06-02 11:50:54 +0200

    added feature graphic (and the script for it)

---
+++ scripts/update-feature-images.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+# Copyright (C) 2023 Andreas Redmer <ar-lato@abga.be>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+todo=`ls metadata/*/images/phoneScreenshots/lato_dawn.png | egrep -o "metadata/.*/images" | egrep -o "/.*/"`
+reps=0
+
+for I in $todo
+do
+  echo "next language is $I"
+  tmpfile=`tempfile -s .png`
+  ls metadata/$I/images/phoneScreenshots/lato_dawn.png
+  #example: convert metadata/en-GB/images/phoneScreenshots/lato_dawn.png -crop 1024x500+128+220\!  metadata/en-GB/images/featureGraphic.png
+  # crop (vertical offset works for both font types)
+  convert metadata/$I/images/phoneScreenshots/lato_dawn.png -crop 1024x500+128+195\! $tmpfile
+  # convert transparant slingerband to dark brown
+  convert $tmpfile -background "#552200" -alpha remove $tmpfile
+
+  compare -verbose -metric PSNR metadata/$I/images/featureGraphic.png $tmpfile /tmp/c.png
+
+  di=$?
+  
+  if [ $di == "0" ]
+  then
+    echo "no feature graphic updates found"
+  else
+    echo "new feature graphic updates found. updating file. commit again"
+    cp $tmpfile metadata/$I/images/featureGraphic.png
+    #exit 1
+  fi
+
+
+  rm -f $tmpfile
+done
+
+
